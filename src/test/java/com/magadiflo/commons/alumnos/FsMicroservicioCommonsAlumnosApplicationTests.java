b9e5e56cbd5eea434bde0889310ef6d8ba457962
Martín <magadiflo@gmail.com>
2022-11-11 10:06:55 -0500

    Proyecto base - con dependencia: Spring Data JPA

---
+++ src/test/java/com/magadiflo/commons/alumnos/FsMicroservicioCommonsAlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magadiflo.commons.alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsMicroservicioCommonsAlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
